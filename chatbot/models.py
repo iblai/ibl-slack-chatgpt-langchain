e844f215504cee0ad8320d826ebb5e2bc3653007
Juan Carlos Espinoza Zurita <carlos_jcez@hotmail.com>
2023-03-20 12:11:54 -0500

    add models

--- chatbot/models.py
+++ chatbot/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from chatbot.utils import count_tokens
 
-# Create your models here.
+class MessageClient(models.Model):
+    conversation_id = models.TextField()
+
+    def get_history(self, max_token=400):
+        history = ""
+        start = "Here is our chat history. We were talking about the things below:\n"
+        for m in self.messages.all():
+            parsed = f"Question: {m.question}\nAnswer: {m.answer}\n\n"
+            if count_tokens(parsed + start) > max_token:
+                break
+            history += parsed
+        if history:
+            history = start + history
+        return history
+    
+
+
+class MessageEntry(models.Model):
+    question = models.TextField()
+    answer = models.TextField()
+    answered_at = models.DateTimeField(auto_now_add=True)
+    client = models.ForeignKey(
+        MessageClient, on_delete=models.CASCADE, related_name="messages"
+    )
